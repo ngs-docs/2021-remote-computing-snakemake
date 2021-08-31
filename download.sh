8705cb00ab671dcdddaa0e35b359e648e7184fc3
C. Titus Brown <titus@idyll.org>
2021-08-31 07:48:21 -0700

    add download

---
+++ download.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+curl -L https://osf.io/5daup/download -o ERR458493.fastq.gz
+curl -L https://osf.io/8rvh5/download -o ERR458494.fastq.gz
+curl -L https://osf.io/xju4a/download -o ERR458500.fastq.gz
+curl -L https://osf.io/nmqe6/download -o ERR458501.fastq.gz
